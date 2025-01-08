0bc48c349a1a9e223569ddbe8abf6bc04c43558e
seojin <sg8525279@gmail.com>
2025-01-08 13:28:15 +0900

    1

--- code.java
+++ code.java
@@ -1,8 +1,7 @@
-class FirstJavaProgram
-{
-    public static void main(String[] args)
-    {
+class FirstJavaProgram {
+    public static void main(String[] args) {
         System.out.println("Welcome to Java");
         System.out.println("First Java program");
+        System.out.println("2 + 5 = " + (2 + 5));
     }
 }
