130201e4690704d2471109bd3b7b501649d87d35
seojin <sg8525279@gmail.com>
2025-01-15 14:34:04 +0900

    BankAccount00

--- code.java
+++ code.java
@@ -1,7 +1,32 @@
-class FirstJavaProgram {
+class BankAccount {
+    int balance = 0;
+
+    public int deposit(int amount) {
+        balance += amount;
+        return balance;
+    }
+    public int withdraw(int amount) {
+        balance -= amount;
+        return balance;
+    }
+    public int checkMyBalance() {
+        System.out.println("잔액 : " + balance);
+        return balance;
+    }
+}
+
+class BankAccount00 {
     public static void main(String[] args) {
-        System.out.println("Welcome to Java");
-        System.out.println("First Java program");
-        System.out.println("2 + 5 = " + (2 + 5));
+        BankAccount yoon = new BankAccount();
+        BankAccount park = new BankAccount();
+
+        yoon.deposit(5000);
+        park.deposit(3000);
+
+        yoon.withdraw(2000);
+        park.withdraw(2000);
+
+        yoon.checkMyBalance();
+        park.checkMyBalance();
     }
 }
