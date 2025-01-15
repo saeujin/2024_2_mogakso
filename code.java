6991f0c80eef7d0bc37848213b9a4f080c45efda
seojin <sg8525279@gmail.com>
2025-01-15 14:36:21 +0900

    DupRef

--- code.java
+++ code.java
@@ -15,18 +15,16 @@ class BankAccount {
     }
 }
 
-class BankAccount00 {
+class DupRef {
     public static void main(String[] args) {
-        BankAccount yoon = new BankAccount();
-        BankAccount park = new BankAccount();
+        BankAccount ref1 = new BankAccount();
+        BankAccount ref2 = ref1; // ref1이 참조하는 대상을 ref2도 참조
 
-        yoon.deposit(5000);
-        park.deposit(3000);
-
-        yoon.withdraw(2000);
-        park.withdraw(2000);
-
-        yoon.checkMyBalance();
-        park.checkMyBalance();
+        ref1.deposit(3000);
+        ref2.deposit(2000);
+        ref1.withdraw(400);
+        ref2.withdraw(300);
+        ref1.checkMyBalance();
+        ref2.checkMyBalance();
     }
 }
