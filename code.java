391051b0a38318b9c105af022f401a7b2eb8ac7f
seojin <sg8525279@gmail.com>
2025-01-15 21:31:44 +0900

    BankAccountUniID

--- code.java
+++ code.java
@@ -1,6 +1,12 @@
 class BankAccount {
-    int balance = 0;
-
+    String accNumber; // 계좌번호
+    String ssNumber; // 주민번호
+    int balance = 0; // 예금 잔액
+    public void initAccount(String acc, String ss, int bal) {
+        accNumber = acc;
+        ssNumber = ss;
+        balance = bal; // 계좌 개설 시 예금액으로 초기화
+    }
     public int deposit(int amount) {
         balance += amount;
         return balance;
@@ -10,19 +16,25 @@ class BankAccount {
         return balance;
     }
     public int checkMyBalance() {
-        System.out.println("잔액 : " + balance);
+        System.out.println("계좌번호 : " + accNumber);
+        System.out.println("주민번호 : " + ssNumber);
+        System.out.println("잔    액 : " + balance + "\n");
         return balance;
     }
 }
-
-class PassingRef {
+class BankAccountUniID {
     public static void main(String[] args) {
-        BankAccount ref = new BankAccount();
-        ref.deposit(3000);
-        ref.withdraw(300);
-        check(ref);
-    }
-    public static void check(BankAccount acc) {
-        acc.checkMyBalance();
+        BankAccount yoon = new BankAccount(); // 계좌 생성
+        yoon.initAccount("12-34-89", "990-990-9090990", 10000); // 초기화
+
+        BankAccount park = new BankAccount(); // 계좌 생성
+        park.initAccount("33-55-09", "77088-5959007", 10000); // 초기화
+
+        yoon.deposit(5000);
+        park.deposit(3000);
+        yoon.withdraw(2000);
+        park.withdraw(2000);
+        yoon.checkMyBalance();
+        park.checkMyBalance();
     }
 }
