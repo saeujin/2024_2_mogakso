108cce12abc74f822326032d71454c3a5c724b98
seojin <sg8525279@gmail.com>
2025-02-24 12:42:52 +0900

    BadEncapsulation

--- 09/3.java
+++ 09/3.java
@@ -1,3 +1,44 @@
-public class 3 {
-    
+class SinivelCap {
+    void take() { // 콧물 처치용 캡슐
+        System.out.println("콧물이 싹~ 납니다.");
+    }
+}
+
+class SneezeCap { // 재채기 처치용 캡슐
+    void take() {
+        System.out.println("재채기가 멎습니다.");
+    }
+}
+
+class SnuffleCap { // 코막힘 처치용 캡슐
+    void take() {
+        System.out.println("코가 뻥 뚫립니다.");
+    }
+}
+
+class ColdPatient {
+    void takeSinivelCap(SinivelCap cap) {
+        cap.take();
+    }
+    void takeSneezeCap(SneezeCap cap) {
+        cap.take();
+    }
+    void takeSnuffleCap(SnuffleCap cap) {
+        cap.take();
+    }
+}
+
+class BadEncapsulation {
+    public static void main(String[] args) {
+        ColdPatient suf = new ColdPatient();
+
+        // 콧물 캡슐 구매 후 복용
+        suf.takeSinivelCap(new SinivelCap());
+
+        // 재채기 캡슐 구매 후 복용
+        suf.takeSneezeCap(new SneezeCap());
+
+        // 코막힘 캡슐 구매 후 복용
+        suf.takeSnuffleCap(new SnuffleCap());
+    }
 }
