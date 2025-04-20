391c8c2bd6ef1fa3c4a559312d9015cd35d9bf80
seojin <sg8525279@gmail.com>
2025-04-20 13:22:40 +0900

    MyBusinessMan

---
+++ study.java
@@ -0,0 +1,29 @@
+public class study {
+    public static void main(String[] args) {
+        BusinessMan man = new BusinessMan("YOON", "Hybrid ELD", "staff Eng.");
+        man.tellYourInfo();
+    }
+}
+
+class Man {
+    String name;
+    public void tellYourName() {
+        System.out.println("My name is " + name);
+    }
+}
+
+class BusinessMan extends Man {
+    String company;
+    String position;
+    public BusinessMan(String name, String company, String position) {
+        this.name = name;
+        this.company = company;
+        this.position = position;
+        
+    }
+    public void tellYourInfo() {
+        System.out.println("My company is "+company);
+        System.out.println("My position is "+position);
+        tellYourName(); // Man 클래스를 상속했기 때문에 호출 가능
+    }
+}
