ae532cfdeadeb0291dc74fa9520ed6df2e5938f7
seojin <sg8525279@gmail.com>
2025-05-06 20:58:16 +0900

    ClassMethod

--- C10/study10.java
+++ C10/study10.java
@@ -1,26 +1,28 @@
 package C10;
 
-class Circle {
-    static final double PI = 3.1415;    // 변하지 않는, 참조가 목적인 값
-    private double radius;
-
-    Circle(double rad) {
-        radius = rad;
+class NumberPrinter {
+    private int myNum = 0;
+    static void showInt(int n) {        // 클래스 메소드(static 메소드)
+        System.out.println(n);
     }
-    void showPerimeter() {
-        double peri = (radius * 2) * PI;
-        System.out.println("둘레: " + peri);
+    static void showdouble(double n) {      // 클래스 메소드드
+        System.out.println(n);
     }
-    void showArea() {
-        double area = (radius * radius) * PI;
-        System.out.println("넓이: " + area);
+    void setMyNumber(int n) {       // 인스턴스 메소드드
+        myNum = n;
+    }
+    void showMyNumber() {       // 인스턴스 메소드
+        showInt(myNum);     // 클래스 내부에서 클래스 메소드 호출
     }
 }
 
 public class study10 {
     public static void main(String[] args) {
-        Circle c = new Circle(1.2);
-        c.showPerimeter();
-        c.showArea();
+        NumberPrinter.showInt(20);      // 클래스 이름을 통한 클래스 메소드 호출
+
+        NumberPrinter np = new NumberPrinter();
+        np.showdouble(3.15);
+        np.setMyNumber(75);
+        np.showMyNumber();
     }
 }
