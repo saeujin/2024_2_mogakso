082cff2c6cf5aa230920a7180b0433fcc1656162
seojin <sg8525279@gmail.com>
2025-05-07 23:07:15 +0900

    CarNBoat

--- C10/study10.java
+++ C10/study10.java
@@ -1,28 +1,22 @@
 package C10;
 
-class SC {
-    static final double PI = 3.1415;
+class Car {
+    void myCar() {
+        System.out.println("this is my car");
+    }
+}
 
-    static double add(double n1, double n2) {
-        return n1 + n2;
-    }
-    static double min(double n1, double n2) {
-        return n1 - n2;
-    }
-    static double calCircleArea(double r) {
-        return PI * r * r;
-    }
-    static double calCirclePeri(double r) {
-        return PI * (r * 2);
+class Boat {
+    void myBoat() {
+        System.out.println("This is my boat");
     }
 }
 
 public class study10 {
     public static void main(String[] args) {
-        System.out.println("3 + 4 = " + SC.add(3,4));
-        System.out.println("반지름 2.2, 원의 넓이: " + SC.calCircleArea(2.2) + "\n");
-
-        System.out.println("15 - 7 = " + SC.min(15,7));
-        System.out.println("반지름 5.0, 원의 둘레: " + SC.calCirclePeri(5.0));
-    }
+		Car c = new Car();
+		c.myCar();
+		Boat t = new Boat();
+		t.myBoat();
+}
 }
