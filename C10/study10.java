2dc044d8c450777ce695c04b1c4b5bb09d8c369a
seojin <sg8525279@gmail.com>
2025-05-07 23:08:44 +0900

    CarNBoat2

--- C10/study10.java
+++ C10/study10.java
@@ -4,19 +4,16 @@ class Car {
     void myCar() {
         System.out.println("this is my car");
     }
+    public static void main(String[] args) {
+		Car c = new Car();
+		c.myCar();
+		Boat t = new Boat();
+		t.myBoat();
+    }   
 }
 
 class Boat {
     void myBoat() {
         System.out.println("This is my boat");
     }
-}
-
-public class study10 {
-    public static void main(String[] args) {
-		Car c = new Car();
-		c.myCar();
-		Boat t = new Boat();
-		t.myBoat();
-}
 }
